53f5291fa26f4bb096720c688ad99f82f5117182
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2017-04-28 16:16:54 -0400

    subspace scanning: core function (no brainstorm)

---
+++ functions/scan_subspace.m
@@ -0,0 +1,53 @@
+function [sc, x_all] = scan_subspace(Us, G, nIter, iSeed, isOutOrient)
+
+if nargin<5
+    isOutOrient = 0;
+end
+
+nDip = size(G,3);
+sc = zeros(nDip,1);
+if ~isOutOrient
+    xx = [];
+    for iDip = 1:nDip
+        sc(iDip) = subcorr(G(:,:,iDip), Us, 1);
+    end
+else
+    x_all = zeros(3,nDip,nIter);
+    for iDip = 1:nDip
+        [sc(iDip), x_all(:,iDip,1), ~] = subcorr(G(:,:,iDip), Us, 1);
+    end
+end
+
+if nIter>1
+    m = size(G,1);
+    for iIter = 2:nIter
+        [~,iMax] = max(sc(:,iIter-1));
+        if (iIter==2)&&(iSeed>0)
+            iMax = iSeed;
+        end
+        [~, xx] = subcorr(G(:,:,iMax), Us, 1);
+        G_max = G(:,:,iMax)*xx;
+        % Compute projector
+        P = eye(m) - G_max*pinv(G_max'*G_max)*G_max';
+        % Project away subspace and leadfields
+        Us = P*Us;
+        if ~isOutOrient
+            parfor iDip=1:nDip
+                G(:,:,iDip) = P*G(:,:,iDip);
+                sc(iDip,iIter) = subcorr(G(:,:,iDip), Us, 1);
+            end
+        else
+            parfor iDip = 1:nDip
+                G(:,:,iDip) = P*G(:,:,iDip);
+                [sc(iDip,iIter), x_all(:,iDip,iIter), ~] = subcorr(G(:,:,iDip), Us, 1);
+            end
+        end
+    end
+else
+    sc = [sc sc];
+end
+
+end
+
+
+
