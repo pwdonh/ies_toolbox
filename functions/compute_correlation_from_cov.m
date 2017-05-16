cadbe6055beae12f69e964f371fbd25b8439e9cf
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2017-05-16 15:55:31 -0400

    different outputs for amplitude modulation, continued

--- functions/compute_correlation_from_cov.m
+++ functions/compute_correlation_from_cov.m
@@ -1,9 +1,11 @@
 function corrvals = compute_correlation_from_cov(C1_all, C2_all, Filters)
 
-nSegment = size(C1_all,3);
+nSegment = size(C2_all,3);
 nComp = size(Filters,2);
 
+fprintf('Computing correlation source %5d of %5d\n', 0, nComp);
 for iComp = 1:nComp
+    fprintf([repmat('\b',1,15) '%5d of %5d\n'], iComp, nComp);
     for iSegment = 1:nSegment
         val(iSegment) = Filters(:,iComp)'*C2_all(:,:,iSegment)*Filters(:,iComp);
     end
