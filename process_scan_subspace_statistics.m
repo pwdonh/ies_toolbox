a5f34c44ee8ef0799c9548a5fccb0b602a26800b
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2017-05-23 11:54:31 -0400

    option to compute statistics on the interpolated MRI values

--- process_scan_subspace_statistics.m
+++ process_scan_subspace_statistics.m
@@ -42,7 +42,16 @@ sProcess.nInputs     = 1;
 sProcess.nMinFiles   = 1;
 sProcess.isSeparator = 1;
 
-sProcess.options.niter.Comment = 'Number of iterations:';
+% 
+sProcess.options.nperm.Comment = 'Number of permutation:';
+sProcess.options.nperm.Type    = 'value';
+sProcess.options.nperm.Value   = {1000, '', 0};
+% 
+sProcess.options.isinterp.Comment = 'Run permutation test in MRI volume';
+sProcess.options.isinterp.Type    = 'checkbox';
+sProcess.options.isinterp.Value   = 0;
+%
+sProcess.options.niter.Comment = 'Number of scanning iterations:';
 sProcess.options.niter.Type    = 'value';
 sProcess.options.niter.Value   = {1, '', 0};
 
@@ -58,6 +67,8 @@ end
 %% ===== RUN =====
 function OutputFiles = Run(sProcess, sInput)
 
+nPerm = sProcess.options.nperm.Value{1};
+isInterp = sProcess.options.isinterp.Value;
 nIter = sProcess.options.niter.Value{1};
 
 sProcessAvg = process_average('GetDescription');
@@ -68,6 +79,17 @@ sProcessScan = process_scan_subspace('GetDescription');
 sProcessScan.options.rsspace.Value = 1; % Set dimensions manually
 sProcessScan.options.niter.Value{1} = nIter;
 sProcessScan.options.issave.Value = 0;
+
+%%
+
+if isInterp
+    sMat = in_bst_results(sInput(1).FileName);
+    sSubject = bst_get('Subject', sInput(1).SubjectFile);
+    sMri = in_mri_bst(sSubject.Anatomy(sSubject.iAnatomy).FileName);
+    grid2mri_interp = grid_interp_mri(sMat.GridLoc, sMri, sMat.SurfaceFile, 0);
+end
+    
+%%
 
 for iFile = 1:numel(sInput)
     
@@ -84,18 +106,24 @@ for iFile = 1:numel(sInput)
     end
     sInputScan = bst_process('GetInputStruct', sMat.Options.SubspaceFile);
     sMatReversed = process_scan_subspace('Run', sProcessScan, sInputScan);
+    data = sMat.ImageGridAmp(:,nIter);
+    data_reversed = sMatReversed.ImageGridAmp(:,nIter);
     
+    if isInterp
+        data = tess_interp_mri_data(grid2mri_interp, size(sMri.Cube), data, 1);
+        data_reversed = tess_interp_mri_data(grid2mri_interp, size(sMri.Cube), data_reversed, 1);
+    end
+        
     % save subcorr maps
-    scMaps{1}{iFile} = sMat.ImageGridAmp(:,nIter);
-    scMaps{2}{iFile} = sMatReversed.ImageGridAmp(:,nIter);
+    scMaps{1}{iFile} = data(:);
+    scMaps{2}{iFile} = data_reversed(:);
     
 end
 
-nSignal = size(scMaps{1}{1},1);
 p_crit = [.05 .01 .005 .001];
 
 % Perform permutation test
-Options.nPerm = 5000;
+Options.nPerm = nPerm;
 Options.nDim = 1;
 Options.stat0 = 0;
 Options.alpha = .05;
@@ -104,7 +132,8 @@ Options.func = @(inputs,para) mean([inputs{1}{:}],2);
 Options.isMaximum = 1;
 % Options.two_tailed = 0; % one-tailed since only positive values
 [pval_max, stat_obs, permvals] = permtest_dep2_new(scMaps{1}, scMaps{2}, Options);
-sortperm = sort(max(permvals));
+% sortperm = sort(max(permvals));
+sortperm = sort(permvals);
 for iCrit = 1:numel(p_crit)
     ii = ceil((1-(p_crit(iCrit)/2))*size(permvals,2));
     stat_crit(iCrit) = sortperm(ii);
@@ -121,6 +150,7 @@ AvgMat = in_bst_results(AvgFile{1});
 AvgMat.ImageGridAmp = [AvgMat.ImageGridAmp(:,nIter) AvgMat.ImageGridAmp(:,nIter)];
 AvgMat.StatResults = Results;
 AvgMat.StatResults.ptype = 'pval_max';
+AvgMat.StatResults.isinterp = isInterp;
 bst_save(AvgFile{1}, AvgMat);
 
 OutputFiles = AvgFile;
